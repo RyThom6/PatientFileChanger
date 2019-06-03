ad96c25618f892d16d6e5db87ab6ff88fae85782
RThomas <48458602+RyThom6@users.noreply.github.com>
2019-06-03 18:14:57 +0100

    Update PatientFiles.py

--- PatientFiles.py
+++ PatientFiles.py
@@ -1,60 +1,61 @@
+#Function to ensure user is done
 def anything_else():
-    next = input("Would you like to do anything else? Y/N ")
+    next = input("Would you like to do anything else? Y/N ").upper()
     if next == "Y":
         command = input("What would you like to do? read, change or append? ")
         if command == "read":
             patientID = input("Enter patientID: ")
-            with open("PatientChecker/" + patientID + ".txt") as f:
+            with open(f"PatientChecker/{patientID}.txt") as f:
                 data = f.read()
                 print(data)
             anything_else()
         elif command == "change":
             patientID = input("Enter patientID: ")
-            with open("PatientChecker/" + patientID + ".txt", "r") as f:
+            with open(f"PatientChecker/{patientID}.txt", "r") as f:
                 lines = f.readlines()
                 print(lines)
-            with open("PatientChecker/" + patientID + ".txt", "w") as f:
-                line = int(input("Which line do you want to edit? "))
+            with open(f"PatientChecker/{patientID}.txt", "w") as f:
+                line = int(input("Which line do you want to edit? (Starting from 0) "))
                 add = input("What would you like to add? (Don't forget to rewrite anything that's already in the line ")
-                lines[line] = add + "\n"
+                lines[line] = f"{add}\n"
                 f.write("".join(lines))
             anything_else()
         elif command == "append":
             patientID = input("Enter patientID: ")
-            with open("PatientChecker/" + patientID + ".txt", "r") as f:
+            with open(f"PatientChecker/{patientID}.txt", "r") as f:
                 lines = f.readlines()
                 print(lines)
-            with open("PatientChecker/" + patientID + ".txt", "a+") as f:
+            with open(f"PatientChecker/{patientID}.txt", "a+") as f:
                 add = input("What would you like to add after the last line? ")
-                f.write(add + "\n")
+                f.write(f"{add}\n")
             anything_else()
     if next == "N":
         print("Goodbye!")
-
+#Main code starts
 command = input("What would you like to do? read, change or append? ")
 if command == "read":
     patientID = input("Enter patientID: ")
-    with open("PatientChecker/" + patientID + ".txt") as f:
+    with open(f"PatientChecker/{patientID}.txt") as f:
         data = f.read()
         print(data)
     anything_else()
 elif command == "change":
     patientID = input("Enter patientID: ")
-    with open("PatientChecker/" + patientID + ".txt", "r") as f:
+    with open(f"PatientChecker/{patientID}.txt", "r") as f:
         lines = f.readlines()
         print(lines)
-    with open("PatientChecker/" + patientID + ".txt", "w") as f:
+    with open(f"PatientChecker/{patientID}.txt", "w") as f:
         line = int(input("Which line do you want to edit? "))
         add = input("What would you like to add? (Don't forget to rewrite anything that's already in the line) ")
-        lines[line] = add + "\n"
+        lines[line] = f"{add}\n"
         f.write("".join(lines))
     anything_else()
 elif command == "append":
     patientID = input("Enter patientID: ")
-    with open("PatientChecker/" + patientID + ".txt", "r") as f:
+    with open(f"PatientChecker/{patientID}.txt", "r") as f:
         lines = f.readlines()
         print(lines)
-    with open("PatientChecker/" + patientID + ".txt", "a+") as f:
+    with open(f"PatientChecker/{patientID}.txt", "a+") as f:
         add = input("What would you like to add after the last line? ")
-        f.write(add + "\n")
+        f.write(f"{add}\n")
     anything_else()
